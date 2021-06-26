564bf66a4af5db1174d5745bb694efcd91ce9445
Cheryl <72804288+KayanCheryl@users.noreply.github.com>
2021-06-26 16:09:05 +0800

    adding weather API  and axios

---
+++ src/app.js
@@ -0,0 +1,10 @@
+function showTemperature(response){
+    let currentTemperature=Math.round(response.data.main.temp)
+    let temperatureElement= document.querySelector("#temperature")
+    temperatureElement.innerHTML=`${currentTemperature}`;
+}
+
+let apiKey=`7d18e9a62c58e4e66a95783116ceb8e4`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&appid=${apiKey}`
+
+axios.get(apiUrl).then(showTemperature);
