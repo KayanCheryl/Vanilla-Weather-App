41d7bb0bf256870f5e70308e8180e03f340fbdbf
Cheryl <72804288+KayanCheryl@users.noreply.github.com>
2021-06-27 21:35:29 +0800

    Added Show temperature js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
 let searchForm = document.querySelector("#form")
 
-function showResult(){
-    event.preventDefault();
-    let countrySubmitted = document.querySelector("#location-input")
-    let countryShown = document.querySelector("#place")
-    console.log(countrySubmitted);
-    countryShown.innerHTML =`${countrySubmitted.value}`
-}
-
-searchForm.addEventListener("submit", showResult)
-
 function showTemperature(response){
     let currentTemperature=Math.round(response.data.main.temp)
     let temperatureElement= document.querySelector("#temperature")
     temperatureElement.innerHTML=`${currentTemperature}`;
 }
 
+function showResult(){
+    event.preventDefault();
+    let countrySubmitted = document.querySelector("#location-input")
+    let countryShown = document.querySelector("#place")
+    let country = `${countrySubmitted.value}`
+    countryShown.innerHTML =`${country}`
+    let apiKey=`7d18e9a62c58e4e66a95783116ceb8e4`;
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${country}&units=metric&appid=${apiKey}`
+    axios.get(apiUrl).then(showTemperature);
+}
+
+searchForm.addEventListener("submit", showResult)
+
+
+
 
 let apiKey=`7d18e9a62c58e4e66a95783116ceb8e4`;
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&appid=${apiKey}`
 
-axios.get(apiUrl).then(showTemperature);
