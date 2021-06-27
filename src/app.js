f80737a1277bf75c83129d7d8e3b2b250c280e7f
Cheryl <72804288+KayanCheryl@users.noreply.github.com>
2021-06-27 22:08:09 +0800

    Added temperature feel like/ humidity/ windSpeed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ function showTemperature(response){
     temperatureElement.innerHTML=`${currentTemperature}`;
 }
 
+function showTemperatureFeelLike(response){
+    let currentTemperatureFeelLike=Math.round(response.data.main.feels_like)
+    let temperatureFeelLikeElement= document.querySelector("#temperatureFeelLike")
+    temperatureFeelLikeElement.innerHTML=`${currentTemperatureFeelLike}`;
+}
+
+function showHumidity(response){
+    let currentHumidity=response.data.main.humidity
+    let humidityElement= document.querySelector("#humidity")
+    humidityElement.innerHTML=`${currentHumidity}`;
+}
+
+function showWindSpeed(response){
+    let currentWindSpeed=response.data.wind.speed
+    let windSpeedElement= document.querySelector("#windSpeed")
+    windSpeedElement.innerHTML=`${currentWindSpeed}`;
+}
+
 function showResult(){
     event.preventDefault();
     let countrySubmitted = document.querySelector("#location-input")
@@ -15,6 +33,9 @@ function showResult(){
     let apiKey=`7d18e9a62c58e4e66a95783116ceb8e4`;
     let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${country}&units=metric&appid=${apiKey}`
     axios.get(apiUrl).then(showTemperature);
+    axios.get(apiUrl).then(showTemperatureFeelLike);
+    axios.get(apiUrl).then(showHumidity);
+    axios.get(apiUrl).then(showWindSpeed);
 }
 
 searchForm.addEventListener("submit", showResult)
@@ -25,3 +46,4 @@ searchForm.addEventListener("submit", showResult)
 let apiKey=`7d18e9a62c58e4e66a95783116ceb8e4`;
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&appid=${apiKey}`
 
+console.log(apiUrl)
